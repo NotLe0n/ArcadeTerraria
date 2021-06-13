0d62a968c9fe2a35932ee87d28b26117968e1dfa
Leon <notLe0n75@gmail.com>
2021-06-13 18:52:31 +0200

    condensed arcade tiles into one file

---
+++ Tiles/ArcadeTiles.cs
@@ -0,0 +1,33 @@
+﻿using ArcadeTerraria.Games;
+using ArcadeTerraria.Games.Game_of_Life;
+using ArcadeTerraria.Games.Minesweeper;
+using ArcadeTerraria.Games.Snake;
+using ArcadeTerraria.Games.Tetris;
+using Microsoft.Xna.Framework;
+
+namespace ArcadeTerraria.Tiles
+{
+    class LifeGameArcade : ArcadeTile
+    {
+        protected override TerrariaGame Game => new LifeGame();
+    }
+
+    class MinesweeperArcade : ArcadeTile
+    {
+        protected override TerrariaGame Game => new MineSweeperGame();
+    }
+
+    class SnakeArcade : ArcadeTile
+    {
+        protected override TerrariaGame Game => new SnakeGame();
+    }
+
+    class TetrisArcade : ArcadeTile
+    {
+        protected override TerrariaGame Game => new TetrisGame
+        {
+            backgroundColor = Color.Black,
+            scale = 2
+        };
+    }
+}
